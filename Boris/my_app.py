de5ac3b41520d5e7431e894270cef28f08773ae0
Boris Dudkin <boris.dudkin@gmail.com>
2023-01-26 22:18:50 -0500

    api calls included

--- Boris/my_app.py
+++ Boris/my_app.py
@@ -2,15 +2,24 @@ import numpy as np
 import pandas as pd
 import sqlalchemy
 import streamlit as st
+import sys
+from pathlib import Path
+import plotly.graph_objects as go
+import plotly.express as px
+sys.path.append('..\\Brian\\')
+sys.path.append('..\\Adam\\')
 
-import math
+# import math
 from Score_calculator.questionnaire_st import get_bucket
 from Sector_name.sector_industry import get_sector_industry_weights
-from Static_data.static_data import step
+from Static_data.static_data import step, capacity_questions
+from MonteCarlo.monte_carlo_input import get_MC_input
+from API_calls.API_calls import get_api_data
 
-
+#create internet page name
 st.set_page_config(page_title="Investment Advisor 💲")
 
+#create four tabs to display data as per below breakdown
 tab1, tab2, tab3, tab4 = st.tabs(['About','Portfolios','Past Performance','Future Returns'])
 
 #tab 1 will contain introduction
@@ -32,13 +41,19 @@ with tab1:
     with st.expander("Funds Description and Risk profile"):
         st.write("Assets in our funds range from High Growth and Crypto to Value stocks and Fixed Income securities of Long term and Short-term maturities. Each fund is constructed with the risk profile of an investor in mind. Our funds are non-diversified and may experience greater volatility than more diversified investments. To compensate for the limited diversification, we only offer Large Cap US equities and Domestic stocks and bonds to reduce volatility brought by small- and medium-cap equities and excluding foreign currency exposure. And yet, there will always be risks involved with ETFs' investments, resulting in the possible loss of money")
 
+# #Retrieve historical data:
+# csvpath = Path("./Resources/historical_data.csv")
+# historical_data_df = pd.read_csv(csvpath, index_col="index", parse_dates=True, infer_datetime_format=True)
+
+#Section below will create a connection to our database and upload relevant tables to dataframes for further analysis
+
 # Database connection string to the clean NYSE database
 database_connection_string = 'sqlite:///Resources/investor.db'
 
 # Create an engine to interact with the database
 engine = sqlalchemy.create_engine(database_connection_string)
 
-capacity_tables=['Q1_Capacity','Q2_Capacity','Q3_Capacity','Q4_Capacity','Q5_Capacity']
+capacity_tables=capacity_questions
 risk_capacity=[]
 for table in capacity_tables:
     pd_name= table+"_df"
@@ -59,9 +74,9 @@ portfolios_df=portfolios_df.set_index('Risk_tolerance')
 sectors_mapping_df =  pd.read_sql_table('sector_mapping', con=engine)
 sectors_mapping_df=sectors_mapping_df.set_index('Sector')
 
+#Subheader will display the questionnaire to determine the risk tolearance and risk capacity profiles of an investor:
 
 with st.sidebar:
-      
     
     st.header('Questionnaire:')
     
@@ -85,7 +100,9 @@ with st.sidebar:
         resp=st.radio(question.index[0], tuple(list(question)))
         score= question.loc[question.index[0],resp]
         c_score+=score
-    
+
+# Section below calculates capacity risk and tolerance risk scores based on the answeres of the investor and generates two portfolios based on those scores:
+
 # tolerance_score= round((score_1+score_2+score_3+score_4+score_5)/len(risk_tolerance_df.index),2)
 tolerance_score=round(t_score/len(risk_tolerance_df.index),2)
 tolerance=get_bucket(tolerance_score, step)
@@ -93,7 +110,10 @@ tolerance=get_bucket(tolerance_score, step)
 capacity_score= round(c_score/len(risk_capacity),2)
 capacity=get_bucket(capacity_score, step)
 
-#tab 2 will contain the selected portfolios and their composition
+# In addition to the capacity risk and tolerance risk based portfolios, cryptomix new etf and the benchmark fund will be added for further analysis:
+
+#tab 2 will display the selected portfolios and their composition (an option to select a portfolio for detailed review will be given to the investor):
+
 with tab2:
 
     st.header('Assessment of the scores and corresponding selected portfolios:')
@@ -133,6 +153,7 @@ with tab2:
     Name_breakdown_df = four_portfolios_dict[portfolio_selection][sectors_mapping_df.columns.to_list().index('Name')+1]
     Market_cap_breakdown_df = four_portfolios_dict[portfolio_selection][sectors_mapping_df.columns.to_list().index('Market Cap & Style')+1]
     
+
     st.markdown('**Portfolio composition by**:')
     col1, col2, col3 = st.columns(3)
     with col1:
@@ -148,4 +169,38 @@ with tab2:
     #select the last element in the breakdown list that contains the dataframe of all the fund characteristics:
     st.write('**:blue[Summary Table]**')
     st.table(four_portfolios_dict[portfolio_selection][-1])
-    
+
+# This section coveres getting ready and making an API call to extract the pricing data for our selected portfolios (API call function accepts the dictionary of tickers and returns prices in the format needed for Monte carlo simulation and daily returns needed for past performance analysis):
+
+# #create a dictionary of tickers based on the index of the four_portfolios_df dataframe:
+# tickers = list(four_portfolios_df.index)
+# portfolios_dict={}
+# for ticker in tickers:
+#     portfolios_dict[ticker]=1/len(tickers)
+
+
+# #get an input for Monte Carlo simulation (note: price data per portfolio will be created at a later stage):
+# Monte_Carlo_df=API_call['Historical prices']
+
+# #get daily pct changes for historical performance analysis:
+# daily_returns_df=API_call['Daily returns']
+
+
+# create an API call and store data
+tickers = list(four_portfolios_df.index)
+n_days=10
+timeframe='1Day'
+api_call_df=get_api_data(tickers, n_days, timeframe)
+
+# inputs to Monte Carlo instance:
+# portfolios df: four_portfolios_df; initial investment: initial_investment; time horizon: time_horizon 
+
+# get_MC_input function creates a list of inputs to create a Monte Carlo class instance, where:
+# Monte_Carlo_df: dataframe of prices;
+# four_portfolios_df: dataframe of our selected portfolios - in the function it will be used to get weights input to Monte Carlo class;
+# portfolio: we create an instance for each portfolio and also use portfolio tickers to select price data from Monte_Carlo_df
+Monte_Carlo__list=[get_MC_input(api_call_df, four_portfolios_df, portfolio, initial_investment, time_horizon) for portfolio in four_portfolios]
+
+
+with tab3:
+    st.write(Monte_Carlo__list[3][1])
